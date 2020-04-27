b57a27f3735c8ae929e48cedbc5058f8d4478001
Marco Antonio Souza <maps@ic.ufal.br>
2020-04-26 23:44:19 -0300

    adição do getpass para tratamento de input de senha

--- check_pw.py
+++ check_pw.py
@@ -1,4 +1,5 @@
 import sys
+import getpass
 from http_request import pwned_api_pw
 from hash_password import hash_txt_pw
 
@@ -7,10 +8,9 @@ def main():
 	arg_count = len(sys.argv)
 
 	if arg_count == 1:
-		print('O argumento senha precisa ser passado')
-		exit()
-
-	arg_pw = sys.argv[1]
+		arg_pw = getpass.getpass(prompt='Digite a senha: ')
+	else:
+		arg_pw = sys.argv[1]
 
 	hex = hash_txt_pw(arg_pw)
 	arr = pwned_api_pw(hex[:5])

cb5ba820a91bc9360bcdff5898328e6c0ba00961
Marco Antonio Souza <maps@ic.ufal.br>
2020-04-27 01:28:42 -0300

    flag encode

--- check_pw.py
+++ check_pw.py
@@ -1,4 +1,3 @@
-import sys
 import getpass
 import argparse
 
@@ -9,24 +8,31 @@ msg = {
 	'input_pass': 'Digite a senha: ',
 	'output_not_found': 'Senha não encontrada',
 	'output_found': 'Senha encontrada.',
-	'output_count': ' Número de ocorrências: '
+	'output_count': 'Número de ocorrências: ',
+	'help_encode': 'Flag indicando se a senha passada já é uma HASH SHA1',
+	'help_password': ''
 }
 
 parser = argparse.ArgumentParser(description='CheckPassword')
-parser.add_argument('--password', '-p', nargs='?')
+parser.add_argument('--password', '-p', nargs='?', help=msg.get('help_password'))
+parser.add_argument('--encode', '-e', nargs='?', type=bool, default=False, choices=[True, False], help=msg.get('help_encode'))
 
 
 def main():
 	args = parser.parse_args()
 
-	arg_pw = args.password if args.password else getpass.getpass(prompt=msg.get('input_pass'))
+	if args.password: arg_pw = args.password
+	else: getpass.getpass(prompt=msg.get('input_pass'))
 
-	hex = hash_txt_pw(arg_pw)
+	if args.encode: hex = arg_pw
+	else: hex = hash_txt_pw(arg_pw)
+
 	arr = pwned_api_pw(hex[:5])
 
 	pw_count = arr.get(hex[5:])
 
-	print(msg.get('output_not_found')) if not pw_count else print(msg.get('output_found') + msg.get('output_count') + pw_count)
+	if not pw_count: print(msg.get('output_not_found'))
+	else: print(msg.get('output_found') + ' ' + msg.get('output_count') + pw_count)
 
 
 if __name__ == '__main__':

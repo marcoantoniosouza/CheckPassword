f9a191f3ad0c588837e31a251d8484e50631bf25
Marco Antonio Souza <maps@ic.ufal.br>
2020-04-27 00:57:18 -0300

    mudança de ifs para operadores ternários e dicionário de mensagens

--- check_pw.py
+++ check_pw.py
@@ -5,6 +5,13 @@ import argparse
 from http_request import pwned_api_pw
 from hash_password import hash_txt_pw
 
+msg = {
+	'input_pass': 'Digite a senha: ',
+	'output_not_found': 'Senha não encontrada',
+	'output_found': 'Senha encontrada.',
+	'output_count': ' Número de ocorrências: '
+}
+
 parser = argparse.ArgumentParser(description='CheckPassword')
 parser.add_argument('--password', '-p', nargs='?')
 
@@ -12,20 +19,15 @@ parser.add_argument('--password', '-p', nargs='?')
 def main():
 	args = parser.parse_args()
 
-	if args.password == None:
-		arg_pw = getpass.getpass(prompt='Digite a senha: ')
-	else:
-		arg_pw = args.password
+	arg_pw = args.password if args.password else getpass.getpass(prompt=msg.get('input_pass'))
 
 	hex = hash_txt_pw(arg_pw)
 	arr = pwned_api_pw(hex[:5])
 
 	pw_count = arr.get(hex[5:])
 
-	if pw_count == None:
-		print("Senha não encontrada")
-	else:
-		print("Senha encontrada. Número de ocorrências: " + pw_count)
+	print(msg.get('output_not_found')) if not pw_count else print(msg.get('output_found') + msg.get('output_count') + pw_count)
+
 
 if __name__ == '__main__':
 	main()
